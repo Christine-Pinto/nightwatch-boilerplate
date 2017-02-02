635e35cd7d442922c721adbda44f3eaa9c317eec
Christine Zierold <christine.zierold@aoe.com>
2017-02-02 14:39:58 +0100

    changed th example tests and also added exports for the new custom commands in the globals.js

--- tests/globals.js
+++ tests/globals.js
@@ -7,8 +7,18 @@ var reporter = new HtmlReporter({
 });
 
 module.exports = {
+    logger: require('./customCommands/logger.js'),
+    scrollToClick: require('./customCommands/scrollToClick'),
+    scrollToSelector: require('./customCommands/scrollToSelector'),
+
     reporter : function(results, done) {
         reporter.fn(results, done);
         done();
+    },
+    afterEach: function(browser, done) {
+        browser.end();
+        setTimeout(function() {
+            done();
+        }, 200);
     }
 };
--- tests/src/examples/google.js
+++ tests/src/examples/google.js
@@ -1,25 +1,24 @@
 module.exports = {
     tags: ['simpletest'],
-    disabled: true,
+    disabled: false,
     'Demo test Google' : function (client) {
         client
-            .url('http://google.no')
+            .url('https://www.google.no/')
             .pause(2000);
 
         client.expect.element('body').to.be.present;
 
+        client.logger('Check Attributes');
         client.expect.element('body').to.have.attribute('class').which.contains('vasq');
         client.expect.element('body').to.have.attribute('class').which.matches(/vasq$/);
         client.expect.element('body').to.have.attribute('class').before(1000);
 
-        client.expect.element('#hplogo').text.to.match(/Norwa/).before(1000);
+        client.expect.element('#hplogo').text.to.match(/Nor/).before(1000);
 
         client.setValue('#lst-ib', 'Norway').pause(500);
         client.expect.element('#lst-ib').to.have.value.equal('Norway');
         client.expect.element('#lst-ib').to.be.an('input');
         client.expect.element('#lst-ib').to.be.not.selected;
         client.expect.element('#lst-ib').to.be.visible;
-
-        client.end();
     }
 };
