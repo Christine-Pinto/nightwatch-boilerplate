f922bc93ab9c161cb0381883c45304e53b86cb91
christine.zierold <christine.zierold@aoe.com>
2017-03-16 09:47:03 +0100

    New nightwatch-cucumber version removed config options for cucumberArgs . Change config for this requirement and also update package

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,26 +2,30 @@ const seleniumServer = require('selenium-server');
 const chromedriver = require('chromedriver');
 const phantomjs = require('phantomjs-prebuilt');
 const dockerSeleniumHub = 'seleniumhub';
+const argv = require('yargs').argv;
 
+
+if (argv.cucumber) {
+    require('nightwatch-cucumber')({
+        cucumberArgs: ['--require', 'timeout.js', '--require', 'features/step_definitions', '--format', 'pretty', '--format', 'json:features/reports/cucumber.json', 'features']
+    });
+}
 module.exports = {
     selenium : {
         start_process : true,
         server_path : seleniumServer.path,
         log_path : '' ,
-        host : '127.0.0.1',
-        port : 4444,
+        selenium_host : '127.0.0.1',
+        selenium_port : 4444,
         cli_args : {
             'webdriver.chrome.driver' : chromedriver.path,
             'webdriver.ie.driver' :'' ,
             'webdriver.firefox.profile':''
         }
     },
-
     test_settings : {
         default : {
             launch_url : 'http://localhost',
-            selenium_host : '127.0.0.1',
-            selenium_port : 4444,
             silent : true,
             disable_colors: false,
             screenshots: {
@@ -31,44 +35,51 @@ module.exports = {
                 path: 'screenshoots/' // relativ to src directory
             },
             desiredCapabilities : {
-                browserName : 'firefox',
+                browserName : 'chrome',
                 javascriptEnabled : true,
                 acceptSslCerts : true,
                 'phantomjs.binary.path': phantomjs.path
             },
+        },
+        chrome: {
+            desiredCapabilities: {
+                browserName: 'chrome',
+                javascriptEnabled: true,
+                acceptSslCerts: true
+            }
+        },
 
-            chrome: {
-                desiredCapabilities: {
-                    browserName: 'chrome',
-                    javascriptEnabled: true,
-                    acceptSslCerts: true
-                }
-            },
-
-            firefox: {
-                desiredCapabilities: {
-                    browserName: 'firefox',
-                    javascriptEnabled: true,
-                    acceptSslCerts: true
-                }
-            },
-            phantomjs: {
-                desiredCapabilities: {
-                    browserName: 'phantomjs',
-                    javascriptEnabled: true,
-                    acceptSslCerts: true,
-                    'phantomjs.binary.path': phantomjs.path
-                }
-            },
-            local_docker:{
-                selenium_host: dockerSeleniumHub,
-                selenium_port: 4444,
-                start_process : false,
-                desiredCapabilities: {
-                    browserName: 'firefox'
+        firefox: {
+            desiredCapabilities: {
+                browserName: 'firefox',
+                javascriptEnabled: true,
+                acceptSslCerts: true
+            }
+        },
+        phantomjs: {
+            desiredCapabilities: {
+                browserName: 'phantomjs',
+                javascriptEnabled: true,
+                acceptSslCerts: true,
+                'phantomjs.binary.path': phantomjs.path
+            }
+        },
+        local_docker:{
+            selenium_host: dockerSeleniumHub,
+            selenium_port: 4444,
+            start_process : false,
+            desiredCapabilities: {
+                browserName: 'firefox'
+            }
+        },
+        mocha: {
+            test_runner: {
+                type: 'mocha',
+                options: {
+                    ui: 'bdd',
+                    reporter: 'list'
                 }
             }
-
         }
     }
 };
