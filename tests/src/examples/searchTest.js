138701ed813ce155c336e6881781171c4a03e335
christine.zierold <christine.zierold@aoe.com>
2017-03-16 09:35:43 +0100

    changed the structure of the plain nightwatch example tests and also renamed the files

---
+++ tests/src/examples/searchTest.js
@@ -0,0 +1,28 @@
+module.exports = {
+    tags: ['simpletest'],
+    disabled: false,
+
+    'Google Simple Search Test' : function (client) {
+        const searchObject = 'Germany';
+
+        client
+            .url('https://www.google.no/')
+            .waitForElementVisible('#hplogo', 2000);
+
+        client.expect.element('body').to.be.present;
+
+        client.logger('Check Attributes');
+        client.expect.element('body').to.have.attribute('class').which.contains('vasq');
+        client.expect.element('body').to.have.attribute('onload');
+
+        client.expect.element('#hplogo').text.to.match(/Nor/);
+        client.expect.element('#lst-ib').to.be.an('input');
+
+        client.logger('Search for ' + searchObject);
+        client.setValue('#lst-ib', searchObject);
+        client.expect.element('#lst-ib').to.have.value.equal(searchObject);
+        client
+            .keys(client.Keys.ENTER)
+            .waitForElementVisible('#search', 2000);
+    }
+};
